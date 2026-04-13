116d91b67bfbf75d92e870c3a20bb56cccf22bd1
Hassan Mokhtar <hsaad.e@mcst.edu.sa>
2026-04-13 12:14:16 +0200

    remove unwanted files

--- precious_collectibles/settings/stag.py
+++ precious_collectibles/settings/stag.py
@@ -1,19 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = ['80.85.153.112']
-
-ADMINS = (
-    ('hassan mokhtar', 'hassan.mokhtar996@gmail.com'),
-)
-
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'precious_collectibles', 
-    'USER': "nagm_addin_3*gpkqzm",
-    'PASSWORD': "GPbkGLldX3OPQVhcgpJ5M9LVebJA3fm0q52nXYaN4",
-    'HOST': '127.0.0.1', 
-    'PORT': '5432',
-}
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static"
